6ffb7a5498805a8b6316bdf54544ba04edf31cfc
Peter Gille <petergil@absum.me>
2021-02-20 11:45:57 +0100

    Initial version of kyria/petergil keymap

---
+++ keyboards/kyria/keymaps/petergil/rules.mk
@@ -0,0 +1,7 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+#TAP_DANCE_ENABLE = yes     # Enable tap dance functionality
+LEADER_ENABLE = yes        # Enable the Leader Key feature
+UNICODEMAP_ENABLE = yes
+#WPM_ENABLE = yes           # WPM counter
